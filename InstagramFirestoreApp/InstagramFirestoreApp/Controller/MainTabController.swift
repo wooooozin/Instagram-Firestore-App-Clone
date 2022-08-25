09f6d15705257212a7a7aa8cf15e3c5c62ad03c2
wooooozin <wooooozin@gmail.com>
2022-08-25 17:18:41 +0900

    [feat] 프로젝트 설정 및 스토리보드 없이 메인 UI 시작

---
+++ InstagramFirestoreApp/InstagramFirestoreApp/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  InstagramFirestoreApp
+//
+//  Created by 효우 on 2022/08/25.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+}
