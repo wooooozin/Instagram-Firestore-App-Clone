aaccbd8beb1d968e5022a5c8d1b1bfe0ba16b592
wooooozin <wooooozin@gmail.com>
2022-08-26 00:11:36 +0900

    [feat] 탭바 컨트롤러 아이콘 셋업 완료

--- InstagramFirestoreApp/InstagramFirestoreApp/Controller/MainTabController.swift
+++ InstagramFirestoreApp/InstagramFirestoreApp/Controller/MainTabController.swift
@@ -17,20 +17,53 @@ class MainTabController: UITabBarController {
     
 }
 
-    // MARK: - Extension Methods
+    // MARK: - Methods
 
 extension MainTabController {
     
     func configureViewControllers() {
         view.backgroundColor = .systemBackground
         
-        let feedVC = FeedController()
-        let searchVC = SearchController()
-        let imageSelectorVC = ImageSelectorController()
-        let notificationVC = NotificationController()
-        let profileVC = ProfileController()
+        let feedVC = templateNavigationController(
+            unselectedImage: #imageLiteral(resourceName: "home_unselected"),
+            selectedImage: #imageLiteral(resourceName: "home_selected"),
+            rootViewController: FeedController()
+        )
+        let searchVC = templateNavigationController(
+            unselectedImage: #imageLiteral(resourceName: "search_unselected"),
+            selectedImage: #imageLiteral(resourceName: "search_selected"),
+            rootViewController: SearchController()
+        )
+        let imageSelectorVC = templateNavigationController(
+            unselectedImage: #imageLiteral(resourceName: "plus_unselected"),
+            selectedImage: #imageLiteral(resourceName: "plus_unselected"),
+            rootViewController: ImageSelectorController()
+        )
+        let notificationVC = templateNavigationController(
+            unselectedImage: #imageLiteral(resourceName: "like_unselected"),
+            selectedImage: #imageLiteral(resourceName: "like_selected"),
+            rootViewController: NotificationController()
+        )
+        let profileVC = templateNavigationController(
+            unselectedImage: #imageLiteral(resourceName: "profile_unselected"),
+            selectedImage: #imageLiteral(resourceName: "profile_selected"),
+            rootViewController: ProfileController()
+        )
         
         viewControllers = [feedVC, searchVC, imageSelectorVC, notificationVC, profileVC]
+        tabBar.tintColor = .black
+    }
+    
+    func templateNavigationController(
+        unselectedImage: UIImage,
+        selectedImage: UIImage,
+        rootViewController: UIViewController
+    ) -> UINavigationController {
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.image = unselectedImage
+        navigationVC.tabBarItem.selectedImage = selectedImage
+        navigationVC.navigationBar.tintColor = .black
+        return navigationVC
     }
     
 }
