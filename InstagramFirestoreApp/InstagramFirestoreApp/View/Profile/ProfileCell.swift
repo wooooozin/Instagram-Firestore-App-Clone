d605c30468be065987f083aa9338a111b21c0460
wooooozin <wooooozin@gmail.com>
2022-10-31 15:56:20 +0900

    profilePage UI 마무리

--- InstagramFirestoreApp/InstagramFirestoreApp/View/Profile/ProfileCell.swift
+++ InstagramFirestoreApp/InstagramFirestoreApp/View/Profile/ProfileCell.swift
@@ -11,11 +11,22 @@ class ProfileCell: UICollectionViewCell {
     
     // MARK: - Properties
     
+    private let postImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "venom-7")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .lightGray
+        
+        addSubview(postImageView)
+        postImageView.fillSuperview()
     }
     
     required init?(coder: NSCoder) {
--- InstagramFirestoreApp/InstagramFirestoreApp/View/Profile/ProfileHeader.swift
+++ InstagramFirestoreApp/InstagramFirestoreApp/View/Profile/ProfileHeader.swift
@@ -11,15 +11,147 @@ class ProfileHeader: UICollectionReusableView {
     
     // MARK: - Properties
     
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.image = #imageLiteral(resourceName: "profile_unselected")
+        iv.contentMode = .scaleAspectFill
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Dog"
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        return label
+    }()
+    
+    private lazy var editProfileFollowButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Edit Profile", for: .normal)
+        button.layer.cornerRadius = 3
+        button.clipsToBounds = true
+        button.layer.borderWidth = 0.5
+        button.layer.borderColor = UIColor.lightGray.cgColor
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(self, action: #selector(handleEditProfileFollowTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    private lazy var postsLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.attributedText = atrributedStateText(value: 5, label: "posts")
+        return label
+    }()
+    
+    private lazy var followerLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.attributedText = atrributedStateText(value: 2, label: "followers")
+        return label
+    }()
+    
+    private lazy var followingLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.attributedText = atrributedStateText(value: 5, label: "following")
+        return label
+    }()
+    
+    let gridButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "grid"), for: .normal)
+        button.tintColor = UIColor(white: 0, alpha: 0.2)
+        return button
+    }()
+    
+    let listButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "list"), for: .normal)
+        button.tintColor = UIColor(white: 0, alpha: 0.2)
+        return button
+    }()
+    
+    let bookmarkButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: "ribbon"), for: .normal)
+        button.tintColor = UIColor(white: 0, alpha: 0.2)
+        return button
+    }()
+
+    
     // MARK: - Lifecycle
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        backgroundColor = .lightGray
+        backgroundColor = .white
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, left: leftAnchor, paddingTop: 16, paddingLeft: 12)
+        profileImageView.setDimensions(height: 80, width: 80)
+        profileImageView.layer.cornerRadius = 80 / 2
+        
+        addSubview(nameLabel)
+        nameLabel.anchor(top: profileImageView.bottomAnchor, paddingTop: 12)
+        nameLabel.centerX(inView: profileImageView)
+        
+        addSubview(editProfileFollowButton)
+        editProfileFollowButton.anchor(top: nameLabel.bottomAnchor,
+                                       left: leftAnchor,
+                                       right: rightAnchor,
+                                       paddingTop: 16,
+                                       paddingLeft: 24,
+                                       paddingRight: 24)
+        
+        let stackView = UIStackView(arrangedSubviews: [postsLabel, followerLabel, followingLabel])
+        stackView.distribution = .fillEqually
+        
+        addSubview(stackView)
+        stackView.centerY(inView: profileImageView)
+        stackView.anchor(left: profileImageView.rightAnchor, right: rightAnchor,
+                         paddingLeft: 12, paddingRight: 12, height: 50)
+        
+        let topDivider = UIView()
+        topDivider.backgroundColor = .lightGray
+        
+        let bottomDivider = UIView()
+        bottomDivider.backgroundColor = .lightGray
+        
+        let buttonStackView = UIStackView(arrangedSubviews: [gridButton, listButton, bookmarkButton])
+        buttonStackView.distribution = .fillEqually
+        buttonStackView.axis = .horizontal
+        
+        addSubview(topDivider)
+        addSubview(bottomDivider)
+        addSubview(buttonStackView)
+        
+        buttonStackView.anchor(left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, height: 50)
+        topDivider.anchor(top: buttonStackView.topAnchor, left: leftAnchor, right: rightAnchor, height: 0.5)
+        bottomDivider.anchor(left: leftAnchor, bottom: buttonStackView.bottomAnchor, right: rightAnchor, height: 0.5)
+        
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    @objc func handleEditProfileFollowTapped() {
+        print(#function)
+    }
+    
+}
+
+extension ProfileHeader {
+    func atrributedStateText(value: Int, label: String) -> NSAttributedString {
+        let atrributeText = NSMutableAttributedString(string: "\(value)\n", attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
+        atrributeText.append(NSAttributedString(string: label, attributes: [.font: UIFont.systemFont(ofSize: 14),
+                                                                            .foregroundColor: UIColor.lightGray]))
+        return atrributeText
+    }
 }
