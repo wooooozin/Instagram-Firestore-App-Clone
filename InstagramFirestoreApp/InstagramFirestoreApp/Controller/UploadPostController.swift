630f9b789e39781a09a7894ba1d4a02b13512091
wooooozin <wooooozin@gmail.com>
2023-01-04 06:44:13 +0900

    uploadPostController 레이아웃 설정

--- InstagramFirestoreApp/InstagramFirestoreApp/Controller/UploadPostController.swift
+++ InstagramFirestoreApp/InstagramFirestoreApp/Controller/UploadPostController.swift
@@ -11,6 +11,27 @@ class UploadPostController: UIViewController {
     
     // MARK: - Properties
     
+    private let photoImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        iv.image = UIImage(systemName: "person.fill")
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    private let captionTextView: UITextView = {
+        let tv = UITextView()
+        return tv
+    }()
+    
+    private let characterCountLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .lightGray
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.text = "0/100"
+        return label
+    }()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -31,7 +52,27 @@ extension UploadPostController {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Share",
                                                             style: .done,
                                                             target: self,
-                                                            action: #selector(didTapDone) )
+                                                            action: #selector(didTapDone))
+        
+        view.addSubview(photoImageView)
+        photoImageView.setDimensions(height: 180, width: 180)
+        photoImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 8)
+        photoImageView.centerX(inView: view)
+        photoImageView.layer.cornerRadius = 10
+
+        view.addSubview(captionTextView)
+        captionTextView.anchor(top: photoImageView.bottomAnchor,
+                               left: view.leftAnchor,
+                               right: view.rightAnchor,
+                               paddingTop: 16,
+                               paddingLeft: 12,
+                               paddingRight: 12,
+                               height: 64)
+        
+        view.addSubview(characterCountLabel)
+        characterCountLabel.anchor(bottom: captionTextView.bottomAnchor,
+                                   right: view.rightAnchor,
+                                   paddingRight: 12)
     }
     
     @objc func didTapCancel() {
---
+++ InstagramFirestoreApp/InstagramFirestoreApp/Utils/InputTextView.swift
@@ -0,0 +1,12 @@
+//
+//  InputTextView.swift
+//  InstagramFirestoreApp
+//
+//  Created by 효우 on 2023/01/04.
+//
+
+import UIKit
+
+class InputTextView: UITextView {
+    
+}
